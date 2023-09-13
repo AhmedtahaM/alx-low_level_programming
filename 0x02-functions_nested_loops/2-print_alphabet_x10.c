0eb0ba47ef9173610a0da02a3c68b96c6e72bd6a
AhmedtahaM <ahmedtaha.mohamedt@gmail.com>
2023-09-12 19:27:20 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include"maon.h"
+
+/**
+ * print_alphabet_x10 - function that will print alphabet 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+int line, ch;
+for (line = 0; line <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _isalpha - checks if character is a letter 
+ * both lowercase or uppercase
+ *
+ * @c: takes input from other functions.
+ *
+ * Return: 1 is c true else 0
+ */
+
+int _iaslpha(int c)
+{
+of (c >= 97 && c <= 122)
+return (1);
+return (0);
+}
