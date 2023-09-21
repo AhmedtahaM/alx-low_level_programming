8ec633947c4532371f67a87065dd21baeb3f6f11
AhmedtahaM <ahmedtaha.mohamedt@gmail.com>
2023-09-21 12:33:18 -0700

    a

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -3,31 +3,8 @@
 
 /**
  * main - check the code
- * @a: an array of integers
- * @n: the number of elements to swap
- *
- * Return: nothing.
- */
-void print_array(int *a, int n)
-{
-int i;
-
-i = 0;
-while (i < n)
-{
-if (i != 0)
-{
-printf(", ");
-}
-printf("%d", a[i]);
-i++;
-}
-printf("\n");
-}
-
-/**
- * main - check the code
- *
+ *print_array
+ *reverse_array
  * Return: Always 0.
  */
 int main(void)
